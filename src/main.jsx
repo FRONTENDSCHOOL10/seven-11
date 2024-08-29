3876c75d160249df05cf08545a255105139d7192
YSP97 <pus9717@gmail.com>
2024-08-29 14:33:07 +0900

    [Init] 초기 환경설정 - tailwind 적용을 위한 환경설정

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import App from './pages/App.jsx';
+import './styles/main.css';
+
+const container = document.getElementById('root');
+
+if (!container) {
+  console.warn('문서에 "#app" 요소가 존재하지 않습니다.');
+} else {
+  createRoot(container).render(<App />);
+}
