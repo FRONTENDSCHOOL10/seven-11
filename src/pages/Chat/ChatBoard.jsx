19b51bc106ae68679fd5e97ed6de4a99c440d5ce
YSP97 <pus9717@gmail.com>
2024-09-03 17:27:39 +0900

    [Feat] 채팅창 보낸, 받은 메세지 컴포넌트 구현 - 채팅창 보낸, 받은 메세지 컴포넌트 구현 - 채팅창 메세지 보드 컴포넌트 구현

---
+++ src/pages/Chat/ChatBoard.jsx
@@ -0,0 +1,24 @@
+import ChatMessage from './ChatMessage';
+import SentChat from './SentChat';
+import ChatTime from './ChatTime';
+import { memo } from 'react';
+
+function ChatBoard() {
+  return (
+    <div className="flex flex-col gap-2 px-3">
+      <ChatMessage
+        message={
+          '안녕하세요 교수님. 저는 어쩌구학과 어쩌구학번 박윤선이라고 합니다.'
+        }
+        time={'오후 7:16'}
+        userImg={'/favicon.svg'}
+        userName={'박윤선'}
+        id="1"
+      />
+      <ChatTime time={'2024년 9월 3일'} />
+      <SentChat time={'오후 7:16'} message={'그러세요 그럼'} />
+    </div>
+  );
+}
+
+export default memo(ChatBoard);
---
+++ src/pages/Chat/SentChat.jsx
@@ -0,0 +1,21 @@
+import { memo } from 'react';
+import ChatNowTime from './ChatNowTime';
+import { string } from 'prop-types';
+
+SentChat.propTypes = {
+  time: string.isRequired,
+  message: string.isRequired,
+};
+
+function SentChat({ time, message }) {
+  return (
+    <div className="flex flex-row gap-[7px] justify-end">
+      <ChatNowTime time={time} />
+      <div className="bg-primary text-white p-3 rounded-[5px] text-base">
+        {message}
+      </div>
+    </div>
+  );
+}
+
+export default memo(SentChat);
