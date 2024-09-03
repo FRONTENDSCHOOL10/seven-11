7a47fe7b53c2512ac5adbd1aee73d990c0327cc5
vngkt3 <wkdalsgur456@naver.com>
2024-09-03 14:42:28 +0900

    [modify] input 컴포넌트 수정

--- src/components/InputConponent.jsx
+++ src/components/InputConponent.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function InputComponent() {
+InputComponent.propTypes = {
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
+function InputComponent({ placeholder, onChange }) {
   return (
     <section className="flex flex-col items-center justify-center">
-      <div className=" m-3 px-3 py-2 border rounded border-black w-fit">
+      <div className="m-3 px-3 py-2 border rounded border-black w-fit">
         <input
-          className="w-64 h-6 text-sm "
+          className="w-64 h-6 text-sm"
           type="text"
           id="InputComponent"
-          placeholder="텍스트를 입력하세요"
+          placeholder={placeholder}
+          onChange={onChange}
         />
       </div>
     </section>
