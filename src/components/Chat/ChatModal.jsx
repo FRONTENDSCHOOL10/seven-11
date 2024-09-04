6b7f096a956a640dab24c319025076ad1ad93776
YSP97 <pus9717@gmail.com>
2024-09-04 11:40:50 +0900

    [Feat] 채팅창 모달 - 모달 컴포넌트 마크업, 스타일링 - 채팅방 적용 예시(Chatroom.jsx) 추가

---
+++ src/components/Chat/ChatModal.jsx
@@ -0,0 +1,15 @@
+import { memo } from 'react';
+import ChatUser from './ChatUser';
+import ChatExitNav from './ChatExitNav';
+
+function ChatModal() {
+  return (
+    <div className="absolute bg-white w-[266px] h-full top-0 right-0">
+      <h3 className="text-[14px] border-b p-2">참여중인 이웃</h3>
+      <ChatUser userName="박윤선" userImg="/favicon.svg" />
+      <ChatExitNav />
+    </div>
+  );
+}
+
+export default memo(ChatModal);
--- src/pages/Chat/Chatroom.jsx
+++ src/pages/Chat/Chatroom.jsx
@@ -1,14 +1,18 @@
 import ChatHeader from '@/components/Chat/ChatHeader';
 import ChatBoard from '../../components/Chat/ChatBoard';
 import { useParams } from 'react-router-dom';
+import ChatModal from '@/components/Chat/ChatModal';
 
 export function Chatroom() {
   const { roomId } = useParams();
   return (
-    <div>
-      <ChatHeader title={'EUID 피그마 스터디 하실분~'} people={3} />
-      <div>채팅방 ID: {roomId}</div>
-      <ChatBoard />
+    <div className="h-full flex flex-col">
+      <div className="bg-[#46464699] opacity-65 flex-grow">
+        <ChatHeader title={'EUID 피그마 스터디 하실분~'} people={3} />
+        <div>채팅방 ID: {roomId}</div>
+        <ChatBoard />
+      </div>
+      <ChatModal />
     </div>
   );
 }
