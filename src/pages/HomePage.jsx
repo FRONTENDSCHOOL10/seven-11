d15d21674c310ac4eb4ddf3805169f74e6b97cb1
otwaylee <ahyeong858@gmail.com>
2024-09-04 11:47:13 +0900

    [Feat] 셀렉트 버튼 컴포넌트 구현 - HomePage내 테스트코드

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
+import SelectButton from '@/components/SelectButton';
 import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
     <div className="flex gap-3 flex-col">
       <h1>홈페이지</h1>
+      <SelectButton
+        options={[
+          { value: 'anyone', label: '누구나' },
+          { value: 'female', label: '여자만' },
+          { value: 'male', label: '남자만' },
+        ]}
+      />
       <Link to={'board/post'}>게시글 작성</Link>
       <Link to={'study-post'}>모집글 작성</Link>
     </div>
