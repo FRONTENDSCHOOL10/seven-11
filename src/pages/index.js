b9f2d572fcdae801dc76e282d65c2d40f24cd373
YSP97 <pus9717@gmail.com>
2024-09-05 11:37:40 +0900

    [Dict] pages index 파일 추가 - router 수정 - pages index 파일 추가 - Chatroom, QuestionPost default export로 변경

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+export { default as LandingPage } from './LandingPage';
+export { default as SelectCategory } from './SelectCategory';
+export { default as SignUp } from './SignUp';
+export { default as CheckEmail } from './CheckEmail';
+
+export { default as Login } from './Login';
+export { default as HomePage } from './HomePage';
+
+export { default as Search } from './Search';
+export { default as StudyPost } from './StudyPost';
+export { default as QuestionPost } from './QuestionPage/QuestionPost';
+
+export { default as Chatroom } from './Chat/Chatroom';
+export { default as ChatList } from './Chat/ChatList';
+
+export { default as UserProfile } from './Profile/UserProfile';
+export { default as EditProfile } from './Profile/EditProfile';
+export { default as ProfileDetail } from './Profile/ProfileDetail';
