13be8bb4cded2ed37b4a0eeb85a9ecf6da25643f
vngkt3 <wkdalsgur456@naver.com>
2024-09-05 12:05:31 +0900

    [design] 회원가입 css 변경

--- src/components/CheckButton.jsx
+++ src/components/CheckButton.jsx
@@ -7,16 +7,14 @@ CheckButton.propTypes = {
 
 function CheckButton({ label, onClick }) {
   return (
-    <section className="flex flex-col items-center justify-center">
-      <button
-        className=" my-2 border rounded border-gray-300 text-base font-bold w-[295px] h-[38px]  "
-        type="button"
-        id="CheckButton"
-        onClick={onClick}
-      >
-        {label}
-      </button>
-    </section>
+    <button
+      className=" my-2 border rounded border-gray-300 text-base font-bold w-[295px] h-[38px]  "
+      type="button"
+      id="CheckButton"
+      onClick={onClick}
+    >
+      {label}
+    </button>
   );
 }
 
