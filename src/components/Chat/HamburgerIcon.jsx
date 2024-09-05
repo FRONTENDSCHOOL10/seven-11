a98ccec8c5f82485b3085296fb0a115dbb5a7539
YSP97 <pus9717@gmail.com>
2024-09-06 01:19:30 +0900

    [Rename] useChatStore->useChatListStore로 파일명 변경 - HamburgerIcon 경로 변경

--- src/components/Chat/HamburgerIcon.jsx
+++ src/components/Chat/HamburgerIcon.jsx
@@ -1,8 +1,8 @@
-import useChatStore from '@/stores/useChatStore';
+import useChatListStore from '@/stores/useChatListStore';
 import { memo } from 'react';
 
-function HamburgerIcon({}) {
-  const setModal = useChatStore((s) => s.setModalOpen);
+function HamburgerIcon() {
+  const setModal = useChatListStore((s) => s.setModalOpen);
   return (
     <button type="button" onClick={setModal} aria-label="참여중인 이웃 보기">
       <svg className="w-5 h-5">
