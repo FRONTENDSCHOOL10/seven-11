8a30ef4677d89536c5459542daef2858c5ecfb95
YSP97 <pus9717@gmail.com>
2024-09-08 18:00:59 +0900

    [Feat] 카테고리 Navigation 제작 - 카테고리 Navigation 마크업, 스타일링 - useCategoryStore를 통한 상태관리(카테고리 목록, 선택된 카테고리 등)

---
+++ src/stores/useCategoryStore.js
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+import pb from '@/api/pb';
+
+const useCategoryStore = create((set) => ({
+  categories: [], // 유저가 선택한 카테고리 list
+  selectedCategory: null, // nav에서 선택한 카테고리
+
+  // 카테고리 목록을 가져오는 함수
+  fetchCategories: async (userId) => {
+    const user = await pb.collection('users').getOne(userId);
+    const categoryIdArray = user.category;
+    const categories = await Promise.all(
+      categoryIdArray.map((id) => pb.collection('Categories').getOne(id))
+    );
+    set({ categories });
+  },
+
+  // 카테고리 선택시 업데이트하는 함수
+  setSelectedCategory: (category) => set({ selectedCategory: category }),
+}));
+
+export default useCategoryStore;
