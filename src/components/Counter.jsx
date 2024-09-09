0f680816878cd38fcf17e4d3291f04c57e21e815
otwaylee <ahyeong858@gmail.com>
2024-09-09 22:51:04 +0900

    [Feat] 카운터 컴포넌트 - 인원수 2-8명

---
+++ src/components/Counter.jsx
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+function Counter({ min = 2, max = 8 }) {
+  const [count, setCount] = useState(min);
+
+  const handleIncrement = () => {
+    if (count < max) {
+      setCount(count + 1);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (count > min) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div className="w-[90px] h-[22px] flex items-center justify-between space-x-2 ">
+      <button
+        className="flex items-center justify-center w-6 h-6"
+        onClick={handleDecrement}
+        disabled={count <= min}
+        aria-label="인원수 줄이기"
+      >
+        <svg className="w-4 h-4">
+          <use href="/stack.svg#minus" />
+        </svg>
+      </button>
+      <span className="text-base font-normal ">{count}명</span>
+      <button
+        className="flex items-center justify-center w-6 h-6"
+        onClick={handleIncrement}
+        disabled={count >= max}
+        aria-label="인원수 늘리기"
+      >
+        <svg className="w-4 h-4">
+          <use href="/stack.svg#plusButton" />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+};
+
+export default Counter;
