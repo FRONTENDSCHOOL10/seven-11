a98b4f1850b2c5d4b0b5d7e64666993d7c5b935d
otwaylee <ahyeong858@gmail.com>
2024-09-09 21:56:41 +0900

    [Modify] 아이콘 텍스트 컴포넌트 prop 수정

--- src/components/IconTextBig.jsx
+++ src/components/IconTextBig.jsx
@@ -4,12 +4,13 @@ import { memo } from 'react';
 IconTextBig.propTypes = {
   icon: string.isRequired,
   text: string.isRequired,
+  color: string,
 };
 
-function IconTextBig({ icon, text }) {
+function IconTextBig({ icon, text, color }) {
   return (
     <div className="flex items-center space-x-2">
-      <svg className="w-5 h-5">
+      <svg className="w-5 h-5" fill={color}>
         <use href={`/stack.svg#${icon}`} />
       </svg>
       <span className="text-base font-normal">{text}</span>
--- src/components/IconTextSmall.jsx
+++ src/components/IconTextSmall.jsx
@@ -4,12 +4,13 @@ import { memo } from 'react';
 IconTextSmall.propTypes = {
   icon: string.isRequired,
   text: string.isRequired,
+  color: string,
 };
 
-function IconTextSmall({ icon, text }) {
+function IconTextSmall({ icon, text, color }) {
   return (
     <div className="flex items-center space-x-2">
-      <svg className="w-[14px] h-[14px]">
+      <svg className="w-[14px] h-[14px]" fill={color}>
         <use href={`/stack.svg#${icon}`} />
       </svg>
       <span className="text-base font-normal">{text}</span>
