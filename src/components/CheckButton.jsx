82991e275ad5fbfd577b84d7ed1f5458a64b6052
vngkt3 <wkdalsgur456@naver.com>
2024-09-10 10:29:41 +0900

    [Design] 카테고리 선택 페이지 스타일링

--- src/components/CheckButton.jsx
+++ src/components/CheckButton.jsx
@@ -1,14 +1,27 @@
-import { string, func } from 'prop-types';
+import { string, func, node } from 'prop-types';
 
 CheckButton.propTypes = {
-  label: string,
+  label: node,
   onClick: func,
+  textColor: string,
+  bgColor: string,
+  border: string,
+  font: string,
+  rounded: string,
 };
 
-function CheckButton({ label, onClick }) {
+function CheckButton({
+  label,
+  onClick,
+  textColor = 'text-black',
+  bgColor = 'bg-white',
+  border = 'border-gray-300',
+  font = 'text-base',
+  rounded = 'rounded',
+}) {
   return (
     <button
-      className=" my-2 border rounded border-gray-300 text-base font-bold w-[295px] h-[38px]  "
+      className={`border rounded border-gray-300 text-base font-bold w-[295px] h-[38px] ${textColor} ${bgColor} ${border} ${font} ${rounded}`}
       type="button"
       id="CheckButton"
       onClick={onClick}
--- src/pages/SelectCategory.jsx
+++ src/pages/SelectCategory.jsx
@@ -1,12 +1,37 @@
 import { Link } from 'react-router-dom';
 import CategoryButton from '@/components/CategoryButton';
+import { CheckButton } from '@/components';
+import SearchBar from '@/components/SearchBar';
+import LeftIcon from '@/components/LeftIcon';
 
 export default function SelectCategory() {
   return (
     <>
       <h1>카테고리 선택</h1>
       <Link to={'/signup'}>회원가입으로 이동</Link>
+
+      <p className="my-2 flex items-center px-2.5 py-1 gap-2 ">
+        <LeftIcon />
+        <SearchBar location="분야 (이름)로 검색" />
+      </p>
+
       <section className="flex justify-between flex-wrap m-3">
+        <button className="my-4 pointer-events-none ">
+          <CheckButton
+            label={
+              <>
+                🚫 작심하루는{' '}
+                <span className="text-[#E90000]">18세 이상의 성인</span>만 이용
+                가능해요
+              </>
+            }
+            bgColor="bg-[#F0FBFF]"
+            border="border-none"
+            font="text-xs"
+            rounded="rounded-[10px]"
+          />
+        </button>
+
         <CategoryButton smallText="언어" largeText="영어" />
         <CategoryButton smallText="언어" largeText="중국어" />
         <CategoryButton smallText="언어" largeText="일본어" />
@@ -17,6 +42,16 @@ export default function SelectCategory() {
         <CategoryButton smallText="자격증" largeText="한국사" />
         <CategoryButton smallText="디자인" largeText="포토샵" />
         <CategoryButton smallText="기타" largeText="기타" />
+
+        <button className="my-4">
+          <CheckButton
+            label="이대로 저장할래요"
+            textColor="text-white"
+            bgColor="bg-primary"
+            border="border-none"
+            rounded="rounded-[8px]"
+          />
+        </button>
       </section>
     </>
   );
