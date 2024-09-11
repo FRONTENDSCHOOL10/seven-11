3a9478c9e5a2df415520bb392d5cd6cb0fcc3873
otwaylee <ahyeong858@gmail.com>
2024-09-11 15:13:57 +0900

    [Modify] 스토어 수정 및 파일명 변경

---
+++ src/stores/useAuthorStore.js
@@ -0,0 +1,30 @@
+import { create } from 'zustand';
+import pb from '../api/pb.js';
+
+const useAuthorStore = create((set, get) => ({
+  postAuthorId: null,
+
+  setPostAuthorId: (id) => {
+    console.log('postAuthorId:', id);
+    set({ postAuthorId: id });
+  },
+
+  fetchPostAuthorId: async (collectionName, postId) => {
+    try {
+      const post = await pb.collection(collectionName).getOne(postId);
+      set({ postAuthorId: post?.authorId });
+    } catch (error) {
+      console.error(
+        `${collectionName}에서 글쓴이 ID를 가져오는데 실패했습니다:`,
+        error
+      );
+    }
+  },
+
+  isAuthor: (loggedInUserId) => {
+    const { postAuthorId } = get();
+    return loggedInUserId === postAuthorId;
+  },
+}));
+
+export default useAuthorStore;
