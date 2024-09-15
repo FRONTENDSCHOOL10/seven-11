1736eff48e572111fdd6e139cfe3c312e04b4170
otwaylee <ahyeong858@gmail.com>
2024-09-16 02:11:19 +0900

    [Modify] 헬멧, 오류 수정 - 질문 페이지 헬멧 추가 - 로그인페이지 import 오류 주석

--- src/pages/QuestionPage/QuestionPage.jsx
+++ src/pages/QuestionPage/QuestionPage.jsx
@@ -2,6 +2,7 @@ import QuestionList from '@/components/QuestionList';
 import SelectButton from '@/components/SelectButton';
 import useCategoryStore from '@/stores/useCategoryStore';
 import { Suspense } from 'react';
+import { Helmet } from 'react-helmet-async';
 
 export function Component() {
   const { categories } = useCategoryStore();
@@ -15,24 +16,33 @@ export function Component() {
   ];
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="border-b border-gray-300 pl-3 py-2">
-        <SelectButton options={options} />
-      </div>
+    <>
+      <Helmet>
+        <title>작심하루 - 질문게시판</title>
+        <meta
+          name="description"
+          content="질문 게시판을 통해 궁금증에 대한 답변을 찾아 보세요. "
+        />
+      </Helmet>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="border-b border-gray-300 pl-3 py-2">
+          <SelectButton options={options} />
+        </div>
 
-      <QuestionList
-        tag="일본어"
-        title="깊은 복사 얕은..."
-        description="제가 이해한... "
-        timeAgo="15분 전"
-        imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwyXeKDN29AmZgZPLS7n0Bepe8QmVappBwZCeA3XWEbWNdiDFB"
-      />
-      <QuestionList
-        tag="자바스크립트"
-        title="깊은 복사 얕은 복사의 차이를 ..."
-        description="제가 이해한 내용은 얕은 복사는..."
-        timeAgo="15분 전"
-      />
-    </Suspense>
+        <QuestionList
+          tag="일본어"
+          title="깊은 복사 얕은..."
+          description="제가 이해한... "
+          timeAgo="15분 전"
+          imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwyXeKDN29AmZgZPLS7n0Bepe8QmVappBwZCeA3XWEbWNdiDFB"
+        />
+        <QuestionList
+          tag="자바스크립트"
+          title="깊은 복사 얕은 복사의 차이를 ..."
+          description="제가 이해한 내용은 얕은 복사는..."
+          timeAgo="15분 전"
+        />
+      </Suspense>
+    </>
   );
 }
