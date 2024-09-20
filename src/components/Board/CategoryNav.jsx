e272aade1cc9e4286c3b1def93fe46371cfae129
otwaylee <ahyeong858@gmail.com>
2024-09-21 00:17:30 +0900

    [Modify] 카테고리 내비게이션 수정 - 마우스, 트랙패드 좌우이동 - 슬라이드 간격 조정

--- src/components/Board/CategoryNav.jsx
+++ src/components/Board/CategoryNav.jsx
@@ -1,8 +1,9 @@
-import { memo } from 'react';
+import { memo, useRef, useLayoutEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import useCategoryStore from '@/stores/useCategoryStore';
 import clsx from 'clsx';
+import { Mousewheel } from 'swiper/modules';
 
 function CategoryNav() {
   const { categories, selectedCategory, setSelectedCategory } =
@@ -16,10 +17,27 @@ function CategoryNav() {
     return clsx(selectedCategory === id ? 'text-white' : 'text-gray-400');
   };
 
+  // Swiper DOM을 참조하는 ref 생성
+  const swiperRef = useRef(null);
+
+  // 레이아웃 효과로 슬라이드 간격 조정
+  useLayoutEffect(() => {
+    const swiperWrapper = swiperRef.current?.querySelector('.swiper-wrapper');
+    if (swiperWrapper) {
+      swiperWrapper.style.justifyContent = 'space-evenly'; // 슬라이드 사이 균등 간격
+    }
+  }, []);
+
   return (
     <div className="w-full h-[37px] bg-primary flex items-center font-semibold px-4">
-      <Swiper spaceBetween={20} slidesPerView={4}>
-        <SwiperSlide className=" flex items-center justify-center whitespace-nowrap ">
+      <Swiper
+        ref={swiperRef} // Swiper 컴포넌트에 ref 적용
+        modules={[Mousewheel]} // Mousewheel 모듈 추가
+        mousewheel // 마우스 휠 사용 설정
+        slidesPerView={4} // 보이는 슬라이드 개수 설정
+        className="w-full h-full"
+      >
+        <SwiperSlide className="flex items-center justify-center whitespace-nowrap">
           <button
             className={`cursor-pointer ${getTextClass(null)}`}
             onClick={() => setSelectedCategory(null)}
@@ -30,7 +48,7 @@ function CategoryNav() {
         {categories.map((item) => (
           <SwiperSlide
             key={item.id}
-            className="flex items-center justify-center whitespace-nowrap   "
+            className="flex items-center justify-center whitespace-nowrap"
           >
             <button
               className={`cursor-pointer ${getTextClass(item.id)}`}
