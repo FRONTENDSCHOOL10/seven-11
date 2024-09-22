261bed3e7ebaf03cf07fcef8b0d43f0128b9ffc4
YSP97 <pus9717@gmail.com>
2024-09-22 23:20:49 +0900

    [BugFix] 오류 수정 - 아바타 이미지 없을 경우 아바타 변수 처리 수정

--- src/api/getPbImageURL.js
+++ src/api/getPbImageURL.js
@@ -1,3 +1,3 @@
-export default function getPbImageURL(item, fileName = 'img') {
+export default function getPbImageURL(item, fileName = 'avatar') {
   return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${item.id}/${item[fileName]}`;
 }
